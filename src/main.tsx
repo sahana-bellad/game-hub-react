d65e37c4b50bb628fa493387a484ea7df6792aa8
sahana-bella <sahanaballad@gmail.com>
2024-06-04 21:40:31 +0530

    Refactoring - fetch games with React Query

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
-  ReactQueryDevtools,
-  ReactQueryDevtoolsPanel,
+  ReactQueryDevtools
 } from "@tanstack/react-query-devtools";
 import App from "./App";
 import theme from "./theme";
