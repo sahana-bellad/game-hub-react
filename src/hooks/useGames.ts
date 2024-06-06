db2dc1c0c87c761b7c1ecb674100c0314ed8c3f7
sahana-bella <sahanaballad@gmail.com>
2024-06-06 20:33:42 +0530

    Implement Infinite Scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -26,9 +26,10 @@ const useGames = (gameQuery: GameQuery) =>
           page: pageParam,
         },
       }),
-      getNextPageParam : (lastPage, allPages) => {
-        return lastPage.next ?  allPages.length + 1 : undefined; 
-      }
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    staleTime: 24 * 60 * 60 * 1000 //24h
   });
 
 export default useGames;
